601ae45b1402faac8a8e602eb6dae96b770be7db
Ain Tohvri <at@interactive-pioneers.de>
2014-01-13 15:15:00 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,116 @@
+'use strict';
+
+module.exports = function (grunt) {
+  // Load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+  // Show elapsed time at the end
+  require('time-grunt')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed MIT */\n',
+    // Task configuration.
+    clean: {
+      files: ['dist']
+    },
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['src/<%= pkg.name %>.js'],
+        dest: 'dist/jquery.<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/jquery.<%= pkg.name %>.min.js'
+      },
+    },
+    qunit: {
+      all: {
+        options: {
+          urls: ['http://localhost:9000/test/<%= pkg.name %>.html']
+        }
+      }
+    },
+    jshint: {
+      options: {
+        reporter: require('jshint-stylish')
+      },
+      gruntfile: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        src: 'Gruntfile.js'
+      },
+      src: {
+        options: {
+          jshintrc: 'src/.jshintrc'
+        },
+        src: ['src/**/*.js']
+      },
+      test: {
+        options: {
+          jshintrc: 'test/.jshintrc'
+        },
+        src: ['test/**/*.js']
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      src: {
+        files: '<%= jshint.src.src %>',
+        tasks: ['jshint:src', 'qunit']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'qunit']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.server.options.livereload %>'
+        },
+        files: [
+          'example/index.html',
+          '<%= copy.example.dest %>'
+        ]
+      },
+    },
+    connect: {
+      server: {
+        options: {
+          livereload: 35729,
+          hostname: '0.0.0.0',
+          port: 9000,
+          open: 'http://0.0.0.0:9000/example/index.html'
+        }
+      }
+    },
+    copy: {
+      example: {
+        src: '<%= uglify.dist.dest %>',
+        dest: 'example/js/jquery.<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', ['connect', 'clean', 'concat', 'uglify', 'copy']);
+  grunt.registerTask('server', ['connect', 'watch']);
+  grunt.registerTask('test', ['jshint', 'connect', 'qunit']);
+};
--- README.md
+++ README.md
@@ -1,4 +1,33 @@
-tinyscrollbar
-=============
+# Tiny Scrollbar
 
-Fork of Tiny Scrollbar, a lightweight cross browser javascript scrollbar by Maarten Baijs.
+A fork of Maarten Baijs' Tiny Scrollbar http://www.baijs.com/tinyscrollbar
+
+Tiny Scrollbar is a nice and elegant way to enable the scrolling of content on mobile and desktop devices.
+
+## Getting Started
+
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/ain/jquery-tinyscrollbar/master/dist/jquery.tinyscrollbar.min.js
+[max]: https://raw.github.com/ain/jquery-tinyscrollbar/master/dist/jquery.tinyscrollbar.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/tinyscrollbar.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
